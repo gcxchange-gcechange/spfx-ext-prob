c998004dea824896d23f60462a9c5063b6c84e00
doug0102 <shea.wow@hotmail.com>
2025-06-19 07:46:58 -0400

    - find group by matching mail to site name - added cache for revoked access - cleaned up debug logs

--- src/extensions/proB/ProBApplicationCustomizer.ts
+++ src/extensions/proB/ProBApplicationCustomizer.ts
@@ -13,6 +13,9 @@ export interface IProBApplicationCustomizerProperties {
   unlockOnError: boolean
 }
 
+const REVOKE_FLAG = 0;
+const GRANT_FLAG = 1;
+
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class ProBApplicationCustomizer extends BaseApplicationCustomizer<IProBApplicationCustomizerProperties> {
   private _graphClient: MSGraphClientV3;
@@ -22,49 +25,47 @@ export default class ProBApplicationCustomizer extends BaseApplicationCustomizer
     this.overlayLock = new OverlayLock();
 
     if (window.location.href.indexOf(`${window.location.origin}/teams/b`) === 0) {
-      if(this.properties.debug)
-        console.log('Pro B site detected...')
+        this.debugLog('Pro B site detected...')
 
       const cacheVal = SessionCache.get(this.context.pageContext.site.id.toString());
-      if (cacheVal === this.context.pageContext.aadInfo.userId.toString()) {
-        if (this.properties.debug)
-          console.log('Access confirmed via cache.')
+      if (cacheVal === `${this.context.pageContext.aadInfo.userId.toString()}${GRANT_FLAG}`) {
+        this.debugLog('Access confirmed via cache.');
 
         this.overlayLock.destroy();
         return Promise.resolve();
+      } else if (cacheVal === `${this.context.pageContext.aadInfo.userId.toString()}${REVOKE_FLAG}`) {
+        this.debugLog('Access revoked via cache.');
+
+        this.overlayLock.destroy();
+        window.location.href = window.location.origin;
       }
 
       this.overlayLock.lock();
       this._graphClient = await this.context.msGraphClientFactory.getClient('3');
 
       try {
-        const siteDetails = await this._graphClient.api(`/sites/${this.context.pageContext.site.id}`).select('displayName').get();
-        console.log('siteDetails', siteDetails);
+        const siteDetails = await this._graphClient.api(`/sites/${this.context.pageContext.site.id}`).select('name').get();
+        this.debugLog('siteDetails', siteDetails);
 
-        if (siteDetails.displayName) {
+        if (siteDetails.name) {
           const groupsResponse = await this._graphClient
           .api(`/groups`)
-          .filter(`groupTypes/any(c:c eq 'Unified') and startswith(displayName, '${siteDetails.displayName}')`)
+          .filter(`groupTypes/any(c:c eq 'Unified') and startswith(mail, '${siteDetails.name}')`)
           .select('id,displayName,description,mail,visibility')
           .header('ConsistencyLevel', 'eventual')
           .get();
 
-          if(this.properties.debug)
-            console.log('groupsResponse', groupsResponse);
+          this.debugLog('groupsResponse', groupsResponse);
 
           const group = groupsResponse.value[0];
-
-          if(this.properties.debug)
-            console.log('group', group);
+          this.debugLog('group', group);
 
           if (group && group.visibility === 'Public') {
             const groupOwners = await this._graphClient.api(`/groups/${group.id}/owners`).select('id,displayName,userPrincipalName').get();
             const groupMembers = await this._graphClient.api(`/groups/${group.id}/members`).select('id,displayName,userPrincipalName').get();
 
-            if(this.properties.debug) {
-              console.log('groupOwners', groupOwners);
-              console.log('groupMembers', groupMembers);
-            }
+            this.debugLog('groupOwners', groupOwners);
+            this.debugLog('groupMembers', groupMembers);
 
             const owners = groupOwners.value;
             const members = groupMembers.value;
@@ -75,24 +76,21 @@ export default class ProBApplicationCustomizer extends BaseApplicationCustomizer
             const isOwner = owners.some((o: { id: unknown; userPrincipalName: string; }) => o.id === currentUserId || o.userPrincipalName === currentUserEmail);
             const isMember = members.some((m: { id: unknown; userPrincipalName: string; }) => m.id === currentUserId || m.userPrincipalName === currentUserEmail);
 
-            if(this.properties.debug) {
-              console.log('isOwner', isOwner);
-              console.log('isMember', isMember);
-            }
+            this.debugLog('isOwner', isOwner);
+            this.debugLog('isMember', isMember);
 
             if (!isOwner && !isMember) {
-              if(this.properties.debug)
-                console.log('Access revoked.')
+              this.debugLog('Access revoked.')
 
+              SessionCache.set(this.context.pageContext.site.id.toString(), `${this.context.pageContext.aadInfo.userId.toString()}${REVOKE_FLAG}`, this.properties.cacheTime ?? 300000);
               this.overlayLock.unlock();
               window.location.href = window.location.origin;
             }
             else {
               this.overlayLock.destroy();
-              SessionCache.set(this.context.pageContext.site.id.toString(), this.context.pageContext.aadInfo.userId.toString(), this.properties.cacheTime ?? 300000);
+              SessionCache.set(this.context.pageContext.site.id.toString(), `${this.context.pageContext.aadInfo.userId.toString()}${GRANT_FLAG}`, this.properties.cacheTime ?? 300000);
 
-              if(this.properties.debug)
-                console.log('Access confirmed.')
+              this.debugLog('Access confirmed.');
             }
           }
         }
@@ -105,5 +103,14 @@ export default class ProBApplicationCustomizer extends BaseApplicationCustomizer
     }
 
     return Promise.resolve();
+  }
+  
+  private debugLog(text: string, obj?: unknown): void {
+    if (this.properties.debug) {
+      if (text && obj)
+        console.log(text, obj);
+      else
+        console.log(text);
+    }
   }
 }
